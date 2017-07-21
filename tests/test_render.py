7a58cc975aef1e2a66a08eb9f3dfaf29252d0cef
Yun Xu <yunx@zillowgroup.com>
2017-07-20 21:29:01 -0700

    unit tests

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,4 +1,5 @@
 from sanic import Sanic
+from sanic.views import HTTPMethodView
 import jinja2_sanic
 import jinja2
 
@@ -32,5 +33,63 @@ async def test_render_func(test_client):
     assert text == "<html><body><h1>CR7</h1>Soccer</body></html>"
     
 
-async def test_render_class_view_based():
-    pass
+async def test_render_class_view_based(test_client):
+    app = Sanic("test_jinja2_render")
+
+    # setup
+    template = "<html><body><h1>{{Player}}</h1>{{Category}}</body></html>"
+    jinja2_sanic.setup(
+        app,
+        loader=jinja2.DictLoader(
+            {
+                "templates.jinja2": template
+            }
+        )
+    )
+
+    class SimpleView(HTTPMethodView):
+
+        @jinja2_sanic.template("templates.jinja2")
+        async def get(self, request):
+            return {
+                "Player": "CR7",
+                "Category": "Soccer",
+            }
+
+    # register routes
+    app.add_route(SimpleView.as_view(), "/")
+
+    cli = await test_client(app)
+    resp = await cli.get('/')
+    assert resp.status == 200
+    text = await resp.text()
+    assert text == "<html><body><h1>CR7</h1>Soccer</body></html>"
+
+
+async def test_render_func_convert_to_coroutine(test_client):
+    app = Sanic("test_jinja2_render")
+
+    # setup
+    template = "<html><body><h1>{{Player}}</h1>{{Category}}</body></html>"
+    jinja2_sanic.setup(
+        app,
+        loader=jinja2.DictLoader(
+            {
+                "templates.jinja2": template
+            }
+        )
+    )
+
+    @app.route("/")
+    @jinja2_sanic.template("templates.jinja2")
+    def func(request):
+        return {
+            "Player": "CR7",
+            "Category": "Soccer",
+        }
+
+    cli = await test_client(app)
+    resp = await cli.get('/')
+    assert resp.status == 200
+    text = await resp.text()
+    assert text == "<html><body><h1>CR7</h1>Soccer</body></html>"
